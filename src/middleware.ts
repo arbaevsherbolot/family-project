4ab6ad2c3a37e4d1b04b9d985695f6bed56c6ac2
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-09-28 16:30:55 +0600

    Update

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,15 +36,33 @@ export async function middleware(request: NextRequest) {
 
   let user: User | undefined;
 
-  if (token?.user) {
-    user = token?.user;
+  if (user) {
+    cookies.set("page", pathname);
     cookies.set("email", user.email);
   }
 
-  if (user) {
-    cookies.set("page", pathname);
-  } else {
-    request.cookies.getAll().map((cookie) => cookies.delete(cookie.name));
+  if (token?.tokens.access_token) {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/profile`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token?.tokens.access_token}`,
+            baseurl: `${process.env.NEXT_PUBLIC_API_URL}`,
+          },
+        }
+      );
+
+      const responseData = await response.json();
+
+      if (responseData.statusCode === 401) {
+        request.cookies.getAll().map((cookie) => cookies.delete(cookie.name));
+      }
+
+      user = responseData.user;
+    } catch (_) {}
   }
 
   if (user && pathname.startsWith("/login")) {
