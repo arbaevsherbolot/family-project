1b54cc7b881640742eb41e065658c49a56e362d9
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-10-04 23:09:20 +0600

    Added burger menu

--- src/components/layout/header/Header.component.tsx
+++ src/components/layout/header/Header.component.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
 import { links } from "../../../data/links";
 import { ArbaevsLogoSvg } from "../../../assets/svg/index";
 import Account from "../../../components/ui/account/Account.component";
@@ -36,9 +37,31 @@ interface props {
 export default function Header({ user }: props) {
   const pathname = usePathname();
 
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  let navbarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        isOpen &&
+        navbarRef.current &&
+        !navbarRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isOpen]);
+
   return (
     <>
-      <header className={styles.header}>
+      <header ref={navbarRef} className={styles.header}>
         <div className={styles.navbar}>
           <div className={styles.left}>
             <Link href="/">
@@ -62,6 +85,48 @@ export default function Header({ user }: props) {
           </div>
 
           <div className={styles.right}>{user && <Account user={user} />}</div>
+
+          <div className={styles.burger_menu}>
+            <div className={styles.menu_icon}>
+              <input
+                className={
+                  isOpen
+                    ? `${styles.button} ${styles.active}`
+                    : `${styles.button}`
+                }
+                type="button"
+                onClick={() => setIsOpen(!isOpen)}
+              />
+              <div>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className={
+                isOpen ? `${styles.menu} ${styles.active}` : `${styles.menu}`
+              }>
+              {user && <Account user={user} />}
+
+              <div className={styles.links}>
+                {links.map((link, i) => (
+                  <Link
+                    key={i}
+                    href={link.path}
+                    onClick={() => setIsOpen(!isOpen)}
+                    className={
+                      pathname === link.path
+                        ? `${styles.link} ${styles.active}`
+                        : styles.link
+                    }>
+                    {link.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </header>
     </>
